2c335fe0daf59f51e609636b8dd14c24c7a418c1
summer2050 <summer2050@users.noreply.github.com>
2026-07-28 05:14:14 +0000

    z summer2050/hugomeme@95de01d53d0ea31ae0e39e17cee18a26d8ee215a

---
+++ js/config/d4451d55aa58d145bd029262c80915b0.js
@@ -0,0 +1,1 @@
+window.config={"comment":{"enable":true,"expired":false},"print":{"expandAdmonition":true,"expandCode":true,"expandDetails":true,"expandFileTree":false},"tooltip":true,"version":"v0.4.5"};
